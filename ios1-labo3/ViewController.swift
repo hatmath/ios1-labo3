6bda755ab36ead1536a37a2b93f1bfce555ee62c
Mathieu H <mathieuhatin@hotmail.com>
2023-06-29 12:05:30 -0700

    tout fonctionne mais arranger mieux le display pour iPad

--- ios1-labo3/ViewController.swift
+++ ios1-labo3/ViewController.swift
@@ -2,19 +2,18 @@
 //  ViewController.swift
 //  labo3pokemon
 //
-//  Created by Simon Turcotte (Étudiant) on 2023-06-20.
+//  Created by Simon Turcotte et Mathieu Hatin (Étudiants) on 2023-06-20.
 //
 
 import UIKit
 
-//let pokemons = ["pikachu", "ditto", "charizard", "poliwrath"]
 let maxPokemons = 100
 
 class ViewController: UIViewController {
     var counter = 0;
     
     @IBOutlet weak var imageViewer: UIImageView!
-
+    @IBOutlet weak var imageViewerBack: UIImageView!
     @IBOutlet weak var textViewer: UILabel!
     
     override func viewDidLoad() {
@@ -38,12 +37,19 @@ class ViewController: UIViewController {
             ImageDownloader.shared.downloadImage(URL(string: pokemon.imageURL)!) { image in
                 DispatchQueue.main.async {
                     self.imageViewer.image = image
+                    //self.imageViewerBack.image = image
                     self.textViewer.text = """
                         ID: \(pokemon.id)
                         Nom: \(pokemon.name)
                         Poids: \(pokemon.weight) dm
                         Taille: \(pokemon.height) hg
                         """
+                }
+            }
+            
+            ImageDownloader.shared.downloadImage(URL(string: pokemon.imageURLBack)!) { image in
+                DispatchQueue.main.async {
+                    self.imageViewerBack.image = image
                 }
             }
         }
